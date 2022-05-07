462cbeb20475c5b3a43af82e8a9c19dcb226a294
ManviSethi <55999896+ManviSethi@users.noreply.github.com>
2022-05-07 22:58:28 +0530

    first

---
+++ src/MyComponents/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+    <span class="navbar">Weather App</span>
+    </div>
+    </nav>
+  )
+}
